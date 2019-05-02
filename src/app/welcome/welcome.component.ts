e4b3af7f46b111b11ef884920d3ce6c11b11c3df
adam <adam_rotaru@yahoo.com>
2019-05-02 17:59:23 +0200

    Add InstaScan, prototype for QR code reading.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -3,6 +3,9 @@ import { ActivatedRoute } from "@angular/router";
 import { WalletService } from "../services/wallet.service";
 import { LanguageService } from "../services/language.service";
 
+// for using Instascan library (from instascan.min.js)
+declare var Instascan: any;
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -15,11 +18,41 @@ export class WelcomeComponent implements OnInit {
   wallet = this.walletService.wallet;
   isConfigured = this.walletService.isConfigured;
 
+  scanner = null;
+  activeCameraId = null;
+  cameras = [];
+  //scans = [];
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private walletService: WalletService,
     private languageService: LanguageService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    let self = this;
+    let videoElem = document.getElementById('instascan');
+    this.scanner = new Instascan.Scanner({ video: videoElem, scanPeriod: 5 });
+    this.scanner.addListener('scan', function (content, image) {
+      console.log('SCAN', content);
+      self.scans.unshift({ date: +(Date.now()), content: content });
+    });
+  
+    let cameras1 = await Instascan.Camera.getCameras();
+    console.log('cameras1', cameras1.length);
+    if (cameras1.length <= 0) {
+      console.error('No cameras found.');
+    } else {
+      this.cameras = cameras1;
+      console.log('cameras[0].id', this.cameras[0].id);
+      this.selectCamera(this.cameras[0]);
+    }
+  }
+
+  async selectCamera(camera: any) {
+    console.log('selectCamera', camera.id);
+    this.activeCameraId = camera.id;
+    console.log('scanner starting');
+    await this.scanner.start(camera);
+    console.log("camera started", camera.id);
   }
 }
